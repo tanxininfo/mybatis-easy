b015b2c2414674f69aab0da15d4c771345c001a8
dudley <tudhongyu@lafengyun.com>
2023-04-12 03:12:39 +0800

    Initial commit

---
+++ src/main/java/info/tanxin/mybatiseasy/MybatisEasyApplication.java
@@ -0,0 +1,13 @@
+package info.tanxin.mybatiseasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisEasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/tanxin/mybatiseasy/MybatisEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package info.tanxin.mybatiseasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
