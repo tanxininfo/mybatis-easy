d43c6eb5907f4cf049887189e7b95fd1969e6351
dudley <32200707@qq.com>
2023-04-16 23:57:01 +0800

    commit

--- mybatis-easy-core/src/main/java/com/mybatiseasy/core/controller/OrderController.java
+++ mybatis-easy-core/src/main/java/com/mybatiseasy/core/controller/OrderController.java
@@ -24,6 +24,7 @@ public class OrderController {
     public void get(){
 
         Order one = orderMapper.getById(2301010015420437L);
+        log.info("one.sqlSessionFactory={}",one.sqlSessionFactory);
         log.info("one={}", one);
     }
 
