6bc6f22d662290f79431063c532f4fc420926797
dudley <32200707@qq.com>
2023-05-03 08:57:50 +0800

    修改版权信息

--- mybatis-easy-apt/src/main/java/com/mybatiseasy/apt/generate/Utils.java
+++ mybatis-easy-apt/src/main/java/com/mybatiseasy/apt/generate/Utils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2033, 杭州坦信科技有限公司 (https://www.mybatis-easy.com).
+ * Copyright (c) 2023, 杭州坦信科技有限公司 (https://www.mybatis-easy.com).
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
