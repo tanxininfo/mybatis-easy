1f0def80cf3df2983136cbc67294ea28734dc57b
dudley <32200707@qq.com>
2023-06-17 21:56:36 +0800

    修正代码生成文件大小写错误、逻辑删除的非删除变量获取错误。

--- mybatis-easy-test/src/main/java/com/mybatiseasy/test/FileGenerator.java
+++ mybatis-easy-test/src/main/java/com/mybatiseasy/test/FileGenerator.java
@@ -31,11 +31,7 @@ public class FileGenerator {
         String url = "jdbc:sqlite:mybatis-easy-test/sqlite/test.db";
         String username = "";
         String password = "";
-
-        url = "jdbc:mysql://dev.tanxin.info:3306/switch_data?allowMultiQueries=true&useSSL=false&useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&zeroDateTimeBehavior=convertToNull&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT%2B8&nullCatalogMeansCurrent=true";
-        username = "switch";
-        password = "jDieur#*(&3";
-
+ 
         String baseDir =  System.getProperty("user.dir") + "\\mybatis-easy-test\\src\\main\\java";
 
         DataSourceConfig dataSourceConfig = new DataSourceConfig.Builder(url, username, password).build();
